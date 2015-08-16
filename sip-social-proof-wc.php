dfd5507ac9b857874e4b4c06241250c9712e3ad8
shopitpress <shopitpress@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-16 04:35:18 +0000

    Version 1.0.1
    git-svn-id: https://plugins.svn.wordpress.org/sip-social-proof-woocommerce/trunk@1221850 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sip-social-proof-wc.php
+++ sip-social-proof-wc.php
@@ -1,17 +1,16 @@
 <?php
 
 /**
- * The plugin bootstrap file
  *
  * @link              https://shopitpress.com
  * @since             1.0.0
- * @package           sip_social_proof_wc
+ * @package           sip_social_proof_woocommerce
  *
  * @wordpress-plugin
  * Plugin Name:       SIP Social Proof for WooCommerce
  * Plugin URI:        https://shopitpress.com/plugins/sip-social-proof-woocommerce/
  * Description:       Display real time proof of your sales and customers.
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            ShopitPress <hello@shopitpress.com>
  * Author URI:        https://shopitpress.com
  * License:           GPL-2.0+
@@ -23,20 +22,21 @@
 
 /*
 Requires: PHP5, WooCommerce Plugin
-Last updated on:  07-08-2015
+Last updated on:  12-08-2015
 */
 
 if ( !defined( 'ABSPATH' ) ) { 
     exit; // Exit if accessed directly
 }
 
-define( 'SIP_SP_PLUGIN_NAME', 'SIP Social Proof for WooCommerce' );
-define( 'SIP_SP_PLUGIN_SLUG', 'sip-social-proof-woocommerce' );
-define( 'SIP_SP_PLUGIN_VERSION', '1.0.0' );
-define( 'SIP_SP_PLUGIN_PURCHASE_URL', 'https://shopitpress.com/plugins/sip-social-proof-woocommerce/' );
-define( 'SIP_SP_BASENAME', plugin_basename( __FILE__ ) );
-define( 'SIP_SP_DIR', trailingslashit( plugin_dir_path( __FILE__ ) ) );
-define( 'SIP_SP_URI', trailingslashit( plugin_dir_url( __FILE__ ) ) );
+// Define plugin constants
+define( 'SIP_SPWC_PLUGIN_NAME', 'SIP Social Proof for WooCommerce' );
+define( 'SIP_SPWC_PLUGIN_SLUG', 'sip-social-proof-woocommerce' );
+define( 'SIP_SPWC_PLUGIN_VERSION', '1.0.1' );
+define( 'SIP_SPWC_PLUGIN_PURCHASE_URL', 'https://shopitpress.com/plugins/sip-social-proof-woocommerce/' );
+define( 'SIP_SPWC_BASENAME', plugin_basename( __FILE__ ) );
+define( 'SIP_SPWC_DIR', trailingslashit( plugin_dir_path( __FILE__ ) ) );
+define( 'SIP_SPWC_URL', trailingslashit( plugin_dir_url( __FILE__ ) ) );
  
 /**
  * Check if woocommerce plugin is installed or not
@@ -49,27 +49,28 @@ if ( in_array(
 	'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) 
 {
 	
-	require_once( SIP_SP_DIR . 'admin/sip-social-proof-admin.php' );
+	require_once( SIP_SPWC_DIR . 'admin/sip-social-proof-admin.php' );
 
 	//add CSS & JS scripts
-	add_action( 'wp_enqueue_scripts', 'sip_sp_scripts' );
-	function sip_sp_scripts() {		
-		wp_enqueue_script( 'script-name', plugin_dir_url( __FILE__ ) .  'assets/js/app.js', array(), '1.0.0', true );
-		wp_register_style( 'sip-sp-style', plugin_dir_url( __FILE__ ) .'assets/css/style.css', array(), '1.0.0');
-    wp_enqueue_style( 'sip-sp-style' );
+	add_action( 'wp_enqueue_scripts', 'sip_spwc_scripts' );
+	function sip_spwc_scripts() {		
+		wp_enqueue_script( 'script-name-p', plugin_dir_url( __FILE__ ) .  'assets/js/app.js', array(), '1.0.0', true );
+		wp_register_style( 'sip-spwc-style', plugin_dir_url( __FILE__ ) .'assets/css/style.css', array(), '1.0.0');
+    wp_enqueue_style( 'sip-spwc-style' );
 	}
 	
-	// Check if SIP_Social_Proof_WC class exists or not, if exist then load the plugin template page
+	// Check if SIP_Social_Proof_WC class exists or not
+	// if exist then load the plugin template page
 	if ( ! class_exists( 'SIP_Social_Proof_WC' ) ) {	 
 
-	  load_plugin_textdomain( 'sip-sp-plugin', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
+	  load_plugin_textdomain( 'sip-spwc-plugin', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
 		
 		/**
 		 * Main plugin class.
 		 *
 		 * @since 1.0.0
 		 *
-		 * @package sip-social-proof
+		 * @package sip_social_proof_woocommerce
 		 * @author	ShopitPress
 		 */
 		class SIP_Social_Proof_WC
@@ -78,18 +79,18 @@ if ( in_array(
 			public function __construct() {
 			
 				// called just before the woocommerce template functions are included				
-				add_action( 'init', array( &$this, 'sip_sp_template_functions' ), 20 );
+				add_action( 'init', array( &$this, 'sip_spwc_template_functions' ), 20 );
+				register_deactivation_hook( __FILE__, array( 'SIP_Social_Proof_WC_Admin' , 'sip_spwc_deactivate' ) );
 			}
 
-			// include template function	
-			public function sip_sp_template_functions() {
+			// include template functions file	
+			public function sip_spwc_template_functions() {
 				include( 'template-functions.php' );
 			}
 			  
 		}//END class SIP_Social_Proof_WC
 
 	}  //END if
-
 
 	// finally instantiate our plugin class and add it to the set of globals
 	$GLOBALS['sip_social_proof_wc'] = new SIP_Social_Proof_WC ();
